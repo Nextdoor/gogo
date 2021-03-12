1219a77b0381f284ce4943db2810fd9edc9b8f4b
Matt Wise <matt@nextdoor.com>
2021-03-12 07:10:11 -0800

    Clean up the entrypoint so that kms_decrypt errors are not swallowed

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 CONFIG=${CONFIG:-ProductionConfig}
 TITLE=${TITLE:-GoGo}
@@ -13,60 +15,60 @@ binary_data = base64.b64decode(sys.argv[1])
 session = boto3.session.Session()
 client = session.client("kms")
 print(client.decrypt(CiphertextBlob=binary_data)["Plaintext"].decode())
-' $1
+' "$1"
 }
 
-if [[ -n "$DATABASE_URI_KMS" ]]; then
-    export DATABASE_URI=$(kms_decrypt $DATABASE_URI_KMS)
-elif [[ -n "$DATABASE_URI" ]]; then
-    export DATABASE_URI
-else
-    echo "DATABASE_URI_KMS/DATABASE_URI must be set."
-    exit 1
+if [ -n "$DATABASE_URI_KMS" ]; then
+  DATABASE_URI=$(kms_decrypt "$DATABASE_URI_KMS") || exit 1
+  export DATABASE_URI
+fi
+
+if [ -n "${DATABASE_URI}" ]; then
+  echo "DATABASE_URI_KMS/DATABASE_URI must be set."
+  exit 1
 fi
 
 if [[ -z "$AUTH_HEADER_NAME" ]]; then
     # Using Built-In Google OAuth.
     echo "AUTH_HEADER_NAME not set. Configuring Google OAuth."
 
-    if [[ -z "$GOOGLE_CLIENT_ID" ]]; then
+    if [ -z "$GOOGLE_CLIENT_ID" ]; then
         echo "GOOGLE_CLIENT_ID must be set."
         exit 1
     fi
 
-    if [[ -z "$HOSTED_DOMAIN" ]]; then
+    if [ -z "$HOSTED_DOMAIN" ]; then
         echo "HOSTED_DOMAIN must be set."
         exit 1
     fi
 
-    if [[ -n "$GOOGLE_CLIENT_SECRET" ]]; then
-        client_secret=$GOOGLE_CLIENT_SECRET
-    elif [[ -n "$GOOGLE_CLIENT_SECRET_KMS_BLOB" ]]; then
-        client_secret=$(kms_decrypt $GOOGLE_CLIENT_SECRET_KMS_BLOB)
-    else
+    if [ -n "$GOOGLE_CLIENT_SECRET_KMS_BLOB" ]; then
+      GOOGLE_CLIENT_SECRET=$(kms_decrypt "$GOOGLE_CLIENT_SECRET_KMS_BLOB")
+    fi
+
+    if [ -n "$GOOGLE_CLIENT_SECRET" ]; then
         echo "Either GOOGLE_CLIENT_SECRET or GOOGLE_CLIENT_SECRET_KMS_BLOB must be set."
         exit 1
     fi
 
-    sed -e "s/{client_id}/${GOOGLE_CLIENT_ID}/g" \
-        -i /app/resources/client_secrets.json
-    sed -e "s/{client_secret}/${client_secret}/g" \
-        -i /app/resources/client_secrets.json
-    sed -e "s/{hosted_domain}/${HOSTED_DOMAIN}/g" \
-        -i /app/resources/client_secrets.json
+    sed -e "s/{client_id}/${GOOGLE_CLIENT_ID}/g" -i /app/resources/client_secrets.json
+    sed -e "s/{client_secret}/${GOOGLE_CLIENT_SECRET}/g" -i /app/resources/client_secrets.json
+    sed -e "s/{hosted_domain}/${HOSTED_DOMAIN}/g" -i /app/resources/client_secrets.json
 
     export REDIRECT_URI=${BASE_URL}/oauth2/callback
+
     # Using # as the separator to handle / in the uri.
     sed -e "s#{redirect_uri}#${REDIRECT_URI}#g" \
         -i /app/resources/client_secrets.json
 
-    if [[ -n "$SESSION_SECRET_KEY" ]]; then
-        export SESSION_SECRET_KEY=$SESSION_SECRET_KEY
-    elif [[ -n "$SESSION_SECRET_KEY_KMS_BLOB" ]]; then
-        export SESSION_SECRET_KEY=$(kms_decrypt $SESSION_SECRET_KEY_KMS_BLOB)
-    else
-        echo "Either SESSION_SECRET_KEY or SESSION_SECRET_KEY_KMS_BLOB must be set."
-        exit 1
+    if [ -n "$SESSION_SECRET_KEY_KMS_BLOB" ]; then
+      SESSION_SECRET_KEY=$(kms_decrypt "$SESSION_SECRET_KEY_KMS_BLOB")
+    fi
+    export SESSION_SECRET_KEY
+
+    if [ -n "$SESSION_SECRET_KEY" ]; then
+      echo "Either SESSION_SECRET_KEY or SESSION_SECRET_KEY_KMS_BLOB must be set."
+      exit 1
     fi
 fi
 
